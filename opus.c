bf78eba081cc81f1d2e6c1867cdfd31476df7247
TheWisePigeon <zozozozeph@gmail.com>
2024-01-25 13:13:47 +0000

    breh

--- opus.c
+++ opus.c
@@ -16,6 +16,21 @@ char *get_binary_representation(int number) {
   return representation;
 };
 
+int nb_bits(int number){
+  int n = 0;
+  while (number & (~0)) {
+    number = number >> 1;
+    n += 1;
+  }
+  return n;
+}
+
+int add(int a, int b){
+  char *bin_a = get_binary_representation(a);
+  char *bin_b = get_binary_representation(b);
+  return 0;
+}
+
 int main(int argc, char **argv) {
   char *representation = get_binary_representation(9);
   printf("%s", representation);
